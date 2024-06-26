f826c4429f98a163f6b9eb9fac6f45c50659409a
segri12 <120318912+segri12@users.noreply.github.com>
2024-06-26 10:58:19 +0300

    Initial commit

---
+++ Script/Builder.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    [SerializeField] private float _checkDistance;
+    [SerializeField] private Transform _raycastPoint;
+    [SerializeField] private Transform
+}
---
+++ Script/Movement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private readonly string Horizontal = "Horizontal";
+    private readonly string Vertical = "Vertical";
+
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis(Horizontal), 0f, Input.GetAxis(Vertical));
+
+        transform.Translate(_speed * Time.deltaTime * direction);
+    }
+}
